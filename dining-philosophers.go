05cc528036f9b3cc246720e7337781812863df6c
Jakob <jakso@itu.dk>
2024-09-03 19:27:59 +0200

    Works with 5 philosophers eating 3 times each.
    Fixed WaitGroup error so its values are correct. The program executes as intended all the time with 5 philosophers eating 3 times each. However when bumping their consumption to 5 there is a success rate of 90-95% without deadlock. And with further increase, more deadlocks occur.
    
    Co-Authored-By: Alex <79690147+AlexTMIT@users.noreply.github.com>
    Co-Authored-By: sarazj <151615837+sarazj@users.noreply.github.com>

--- dining-philosophers.go
+++ dining-philosophers.go
@@ -27,7 +27,7 @@ func main() {
 }
 
 func initStructs() {
-	for i := 0; i < 3; i++ {
+	for i := 1; i <= 5; i++ {
 		ps = append(ps, p{id: i, ch: make(chan f, 2)})
 		fs = append(fs, f{id: i, ch: make(chan p, 1)})
 	}
@@ -37,39 +37,27 @@ func initThreads() {
 	for _, p := range ps {
 		wg.Add(1)
 		fmt.Printf("*** Starting thread %d\n", p.id)
-		go initPhiloThread(&p)
+		go run(&p)
 	}
-
-	/*for _, f := range fs {
-		wg.Add(1)
-		go initForkThread(f)
-	}*/
 }
 
-func initPhiloThread(p *p) {
+func run(p *p) {
 	defer wg.Done()
-	for p.nom < 5 {
-		fmt.Printf("* Starting iteration of philosopher %d with nom %d\n", p.id, p.nom)
+	for p.nom < 3 {
+		//fmt.Printf("* Starting iteration of philosopher %d with nom %d\n", p.id, p.nom)
 
-		fl := fs[p.id]
-		fr := fs[(p.id+1)%len(ps)]
+		fl := fs[p.id-1]
+		fr := fs[(p.id)%len(ps)]
 
-		fmt.Printf("Philosopher %d is checking left fork%d\n", p.id, fr.id)
+		//fmt.Printf("Philosopher %d is checking left fork%d\n", p.id, fr.id)
 		checkFork(p, fl)
-		fmt.Printf("Philosopher %d is checking right fork%d\n", p.id, fr.id)
+		//fmt.Printf("Philosopher %d is checking right fork%d\n", p.id, fr.id)
 		checkFork(p, fr)
 	}
-}
-
-func initForkThread(f f) {
-	defer wg.Done()
-	// pl := ps[f.id]
-	// pr := ps[(f.id-1)%len(ps)]
+	fmt.Printf("Philosopher %d is done eating.\n", p.id)
 }
 
 func checkFork(p *p, f f) {
-	defer wg.Done()
-	// m := <-f.ch // m is a philosopher
 	if len(f.ch) == 0 {
 		fmt.Printf("Fork %d has no philosopher, so philosopher %d attempts to grab it.\n", f.id, p.id)
 		grabFork(p, f)
@@ -88,11 +76,11 @@ func grabFork(p *p, f f) {
 }
 
 func eat(p *p) {
-	p.nom++                   // eat
-	<-fs[p.id].ch             // leave fl
-	<-fs[(p.id+1)%len(ps)].ch // leave fr
-	<-p.ch                    // leave 1 fork
-	<-p.ch                    // leave other
+	p.nom++                 // eat
+	<-fs[p.id-1].ch         // leave fl
+	<-fs[(p.id)%len(ps)].ch // leave fr
+	<-p.ch                  // leave 1 fork
+	<-p.ch                  // leave other
 	fmt.Printf("Philosopher %d ate. Now he is %d full.\n", p.id, p.nom)
 }
 
