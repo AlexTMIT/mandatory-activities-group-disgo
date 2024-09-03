30838336d4dda75955a101710fc8ba369a76a9da
Jakob <jakso@itu.dk>
2024-09-03 14:04:22 +0200

    Philosophers eat now.
    We still receive one WaitGroup error, however a lot more functionality now works.
    
    Co-Authored-By: Alex <79690147+AlexTMIT@users.noreply.github.com>
    Co-Authored-By: sarazj <151615837+sarazj@users.noreply.github.com>

--- dining-philosophers.go
+++ dining-philosophers.go
@@ -11,13 +11,13 @@ var wg sync.WaitGroup
 
 type p struct {
 	id  int
-	ch  chan (f)
+	ch  chan f
 	nom int
 }
 
 type f struct {
 	id int
-	ch chan (p)
+	ch chan p
 }
 
 func main() {
@@ -28,30 +28,37 @@ func main() {
 
 func initStructs() {
 	for i := 0; i < 3; i++ {
-		ps = append(ps, p{id: i, ch: make(chan f)})
-		fs = append(fs, f{id: i, ch: make(chan p)})
+		ps = append(ps, p{id: i, ch: make(chan f, 2)})
+		fs = append(fs, f{id: i, ch: make(chan p, 1)})
 	}
 }
 
 func initThreads() {
 	for _, p := range ps {
 		wg.Add(1)
-		go initPhiloThread(p)
+		fmt.Println("Starting thread", p.id)
+		go initPhiloThread(&p)
 	}
 
-	for _, f := range fs {
+	/*for _, f := range fs {
 		wg.Add(1)
 		go initForkThread(f)
-	}
+	}*/
 }
 
-func initPhiloThread(p p) {
+func initPhiloThread(p *p) {
 	defer wg.Done()
-	fl := fs[p.id]
-	fr := fs[(p.id+1)%len(ps)]
+	for p.nom < 5 {
+		fmt.Printf("Starting iteration of philosopher %d with nom %d\n", p.id, p.nom)
 
-	go checkFork(p, fl)
-	go checkFork(p, fr)
+		fl := fs[p.id]
+		fr := fs[(p.id+1)%len(ps)]
+
+		fmt.Println("Checking left fork", fr.id)
+		checkFork(p, fl)
+		fmt.Println("Checking right fork", fr.id)
+		checkFork(p, fr)
+	}
 }
 
 func initForkThread(f f) {
@@ -60,16 +67,18 @@ func initForkThread(f f) {
 	// pr := ps[(f.id-1)%len(ps)]
 }
 
-func checkFork(p p, f f) {
-	m := <-f.ch // m is a philosopher
-	if m.ch == nil {
+func checkFork(p *p, f f) {
+	defer wg.Done()
+	// m := <-f.ch // m is a philosopher
+	if len(f.ch) == 0 {
+		fmt.Printf("Fork %d has no philosopher, so philosopher %d attempts to grab it.\n", f.id, p.id)
 		grabFork(p, f)
 	}
 }
 
-func grabFork(p p, f f) {
-	f.ch <- p // push p to f channel
-	p.ch <- f // push f to p channel
+func grabFork(p *p, f f) {
+	f.ch <- *p // push p to f channel
+	p.ch <- f  // push f to p channel
 
 	if len(p.ch) == 2 {
 		eat(p)
@@ -78,7 +87,7 @@ func grabFork(p p, f f) {
 	}
 }
 
-func eat(p p) {
+func eat(p *p) {
 	p.nom++                   // eat
 	<-fs[p.id].ch             // leave fl
 	<-fs[(p.id+1)%len(ps)].ch // leave fr
@@ -87,6 +96,6 @@ func eat(p p) {
 	fmt.Printf("Philosopher %d ate. Now he is %d full.\n", p.id, p.nom)
 }
 
-func think(p p) {
+func think(p *p) {
 	fmt.Printf("Philosopher %d is thinking.", p.id)
 }
