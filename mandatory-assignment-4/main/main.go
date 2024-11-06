7a7c9aacab1f7d4c04c144e80d169301ec0d3cac
Alex Tilgreen Mogensen <alext@itu.dk>
2024-11-06 09:26:44 +0100

    update main.go for running all the time

--- mandatory-assignment-4/main/main.go
+++ mandatory-assignment-4/main/main.go
@@ -4,26 +4,29 @@ import (
 	"consensus/process"
 	"fmt"
 	"strconv"
+	"time"
 )
 
 var n int
 
 func main() {
+	fmt.Print("Hello. How many processes do you want?")
+	fmt.Scanln(&n)
+	var ports []string
+	var entry = "localhost:"
+
+	for i := 0; i < n; i++ {
+		var number = i + 50051
+		var port = entry + strconv.Itoa(number)
+		ports = append(ports, port)
+	}
+
+	for i := 0; i < n; i++ {
+		var port = ports[i]
+		go process.Run(port, ports)
+	}
+
 	for {
-		fmt.Print("Hello. How many processes do you want?")
-		fmt.Scanln(&n)
-		var ports []string
-		var entry = "localhost:"
-
-		for i := 0; i < n; i++ {
-			var number = i + 50051
-			var port = entry + strconv.Itoa(number)
-			ports = append(ports, port)
-		}
-
-		for i := 0; i < n; i++ {
-			var port = ports[i]
-			go process.Run(port, ports)
-		}
+		time.Sleep(100000)
 	}
 }
