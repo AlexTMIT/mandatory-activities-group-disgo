d28c25805de85600c84cb6e33f1ce88bb5c18a3d
Alex Tilgreen Mogensen <alext@itu.dk>
2024-11-21 13:56:13 +0100

    some refacts :D

--- mandatory-assignment-5/client/client.go
+++ mandatory-assignment-5/client/client.go
@@ -14,19 +14,15 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-var running bool
 var name string
-var currentAmount int
+var ctx1, c1 = connectToServer("localhost:50051")
+var ctx2, c2 = connectToServer("localhost:50052")
 
 func main() {
 	setName()
 
-	ctx1, c1 := connectToServer("localhost:50051")
-	ctx2, c2 := connectToServer("localhost:50052")
-
 	for {
-		listenToInput(ctx1, c1)
-		listenToInput(ctx2, c2)
+		listenToInput()
 	}
 }
 
@@ -63,11 +59,9 @@ func join(ctx context.Context, c pb.ReplicationServiceClient) {
 		log.Println("Failed to process join request")
 	}
 	log.Printf("%s", req.Msg)
-	running = true
 }
 
 func bid(ctx context.Context, c pb.ReplicationServiceClient, bid int) {
-	log.Printf("C %s", &c)
 	req, err := c.Bidding(ctx, &pb.BidRequest{Amount: int32(bid), ClientName: name})
 	if err != nil {
 		log.Println("Error in bidding.")
@@ -75,27 +69,42 @@ func bid(ctx context.Context, c pb.ReplicationServiceClient, bid int) {
 	log.Println(req.Response)
 }
 
-func listenToInput(ctx context.Context, c pb.ReplicationServiceClient) {
-	reader := bufio.NewReader(os.Stdin)
+func listenToInput() {
+	command := readCommand()
+	split, keyword := parseCommand(command)
 
+	if keyword == "query" {
+		queryBidding(ctx1, c1)
+		queryBidding(ctx2, c2)
+	} else if keyword == "bid" {
+		processBid(split)
+	}
+}
+
+func processBid(split []string) {
+	currentAmount, err := strconv.Atoi(split[1])
+	if err != nil {
+		fmt.Println("Invalid input.")
+	}
+	log.Printf("bidding amount %d", currentAmount)
+
+	bid(ctx1, c1, currentAmount)
+	bid(ctx2, c2, currentAmount)
+}
+
+func parseCommand(command string) ([]string, string) {
+	split := strings.Split(command, " ")
+	keyword := strings.ToLower(split[0])
+	return split, keyword
+}
+
+func readCommand() string {
+	reader := bufio.NewReader(os.Stdin)
 	command, err := reader.ReadString('\n')
 	if err != nil {
 		log.Printf("Error reading input: %v", err)
-		return
+		return ""
 	}
 
-	command = strings.TrimSpace(command)
-	split := strings.Split(command, " ")
-	keyword := strings.ToLower(split[0])
-
-	if keyword == "query" {
-		queryBidding(ctx, c)
-	} else if keyword == "bid" {
-		currentAmount, err := strconv.Atoi(split[1])
-		if err != nil {
-			fmt.Println("Invalid input.")
-		}
-		log.Printf("bidding amount %d", currentAmount)
-		bid(ctx, c, currentAmount)
-	}
+	return strings.TrimSpace(command)
 }
